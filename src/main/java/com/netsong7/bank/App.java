97e4c482101e8a4939b43103428b65c25aac9634
Jeong Woo <netsong7@naver.com>
2017-01-05 11:39:18 +0900

    test

--- src/main/java/com/netsong7/bank/App.java
+++ src/main/java/com/netsong7/bank/App.java
@@ -1,16 +1,20 @@
 package com.netsong7.bank;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
+	static final Logger logger = LogManager.getLogger(LogTest.class);
+	
 	public static void main(String[] args) {
 		ApplicationContext ctx = 
 			new ClassPathXmlApplicationContext("classpath:META-INF/applicationContext.xml");
 		
 		FixedDepositController controller = 
 			new FixedDepositController();
-		System.out.println("정기예금 개설여부:" + controller.submit());
-		System.out.println("1번고객의 정기예금 정보:" +controller.get(1));
+		logger.info("정기예금 개설여부:" + controller.submit());
+		logger.info("1번고객의 정기예금 정보:" +controller.get(1));
 	}
 }
